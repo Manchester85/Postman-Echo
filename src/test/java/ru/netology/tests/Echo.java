2f440f5332e7406d2c491452452ddbda5dd16fd0
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-10-25 15:53:34 +0300

    test added

---
+++ src/test/java/ru/netology/tests/Echo.java
@@ -0,0 +1,28 @@
+package ru.netology.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Echo {
+    @Test
+    void shouldReturnData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .header("Content-Type",equalTo("application/json; charset=utf-8"))
+                .header("Content-Length", equalTo("419"))
+                .header("Connection", equalTo("keep-alive"));
+    }
+}
